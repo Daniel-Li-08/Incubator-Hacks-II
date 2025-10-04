3b38560f457a90edad3fe9b7d0eb315d7a177f10
Daniel Li <daniel.li.stuff@gmail.com>
2025-10-04 14:58:42 -0400

    Template with Blocker

---
+++ background.js
@@ -0,0 +1,6 @@
+// Background script for future functionality
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Website Blocker extension installed');
+});
+
+// You can add more background functionality here as needed
